ef7b85da7e48983f15b1caca5194e5d8a1f8ba1e
JuliaGoo <116673410+JuliaGoo@users.noreply.github.com>
2023-02-26 23:48:19 +0100

    removed line in js code

--- js/menu.js
+++ js/menu.js
@@ -9,7 +9,6 @@
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
